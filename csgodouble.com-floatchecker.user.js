d518ea2656f0d4d71e85e25defb6bdfddc5bf72d
Mole <aareksio@gmail.com>
2016-01-17 18:52:44 +0100

    Slightly bigger window;

--- csgodouble.com-floatchecker.user.js
+++ csgodouble.com-floatchecker.user.js
@@ -12,7 +12,7 @@
 "use strict";
 
 function checkfloat(ins) {
-    var myWindow = window.open("http://king-dev.com/float.php#" + ins, "", "toolbar=no, scrollbars=no, resizable=no, top=150, left=150, width=320, height=120");
+    var myWindow = window.open("http://king-dev.com/float.php#" + ins, "", "toolbar=no, scrollbars=no, resizable=no, top=150, left=150, width=340, height=120");
 }
 
 $("#contextMenu").append("<li><a tabindex='-1' href=\"javascript:checkfloat($('#contextMenu [data-act=0]').attr('href'))\" data-act='1'>Check Float...</a></li>");
